ae7a781cdd74e6866d76c81bf34c7246fd5a7d62
yernurmaksat <zh.yernur@gmail.com>
2025-08-03 09:38:58 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,15 @@
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8291369010:AAGJEVaOp3ovV88Ol2bpCRDAMbQdVCfiaFU"
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я Telegram-бот. Просто отправь мне текст.")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_text = update.message.text
-    reply = f"Вы написали: {user_text}"
-    await update.message.reply_text(reply)
+    await update.message.reply_text("Бот работает! ✅")
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     app.run_polling()
 
 if __name__ == "__main__":
